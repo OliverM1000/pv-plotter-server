2599a873bb9a84d606779ddf487708b3622ee6ba
Oliver Mueller <oliver.mueller.1000@hotmail.com>
2024-11-13 23:29:24 -0800

    added a second signal. now emitting two different signals.

--- index.js
+++ index.js
@@ -49,10 +49,14 @@ io.on('connection', (socket) =>
 
 
 var counter = 0
-var signal = 0;
+var signal1 = 0;
+var signal2 = 0;
 setInterval(()=>{
     counter++;
-    signal = Math.sin(counter / 100)
-    console.log(`${counter} ${signal}`);
-    io.emit("data", counter, signal);
+    signal1 = Math.sin(counter / 100) * (1 - Math.random()/5);
+    signal2 = Math.cos(counter / 100) * (1 - Math.random()/10);
+    console.log(`${counter} ${signal1} ${signal2}`);
+        
+    io.emit("signal1", counter, signal1);
+    io.emit("signal2", counter, signal2);
 }, 20);
